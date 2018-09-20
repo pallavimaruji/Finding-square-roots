cd0bed7c7d8c08c891965c03f7f531c34562f1fe
Pallavi Agarwal <36764894+pallavimaruji@users.noreply.github.com>
2018-09-20 18:56:34 +0530

    Add files via upload

---
+++ Finding square roots/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        int n;
+        cin>>n;
+        int ans=sqrt(n);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
